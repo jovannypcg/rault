b7068b4ccf1ccc441b8e9a9dcf9e069d3042267b
Jovanny Cruz <jovannypcg@yahoo.com>
2018-05-15 13:31:37 -0500

    First commit

---
+++ src/main/java/mx/jovannypcg/rault/RaultApplication.java
@@ -0,0 +1,12 @@
+package mx.jovannypcg.rault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaultApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(RaultApplication.class, args);
+	}
+}
---
+++ src/test/java/mx/jovannypcg/rault/RaultApplicationTests.java
@@ -0,0 +1,16 @@
+package mx.jovannypcg.rault;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaultApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
